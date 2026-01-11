4eac8d85ea779e4002ba79a5376c3fd281e73363
Rutvikmaida <145426273+Rutvikmaida@users.noreply.github.com>
2026-01-11 20:10:17 +0530

    Scroll

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import About from './componets/about';
 import Education from './componets/education';
 import Project from './componets/project';
 import Contact from './componets/contact';
+import ScrollToTop from './componets/ScrollToTop';
 import './App.css';
-import {BrowserRouter as Router, Route, Routes, BrowserRouter} from 'react-router-dom';
+import {BrowserRouter , Router, Route, Routes} from 'react-router-dom';
 const APP=()=>
 {
 return(
        <>
        <BrowserRouter> 
+         <ScrollToTop />
        <Routes>
         <Route exact path='/' element={<Home/>} />
         <Route exact path='/about' element={<About/>}/>
